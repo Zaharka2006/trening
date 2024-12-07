7650010cb61037cf488bb054d4107fab69276d97
PythonGod <3935420@gmail.com>
2024-12-07 23:16:53 +0300

    add feature discount in func calculate_tax and add tests for this

--- src/taxes.py
+++ src/taxes.py
@@ -1,12 +1,21 @@
-def calculate_tax(price: float , tax_rate: float) -> float:
+def calculate_tax(price: float, tax_rate: float, discount: float = 0) -> float:
     """ Эта функция рассчитывает стоимость товара с учётом указанного налога """
 
     if tax_rate < 0:
         raise ValueError('Неверный налоговый процент')
 
-    if price <= 0:
+    elif price < 0:
         raise ValueError('Неверная цена')
 
-    final_price = price + (price / 100 * tax_rate)
+    elif discount < 0:
+        raise ValueError('Неверная скидка')
 
-    return final_price
+    elif discount > 0:
+        tax_price = price + (price / 100 * tax_rate)
+        discount_price = tax_price - (tax_price / 100 * discount)
+        return discount_price
+
+    else:
+        tax_price = price + (price / 100 * tax_rate)
+
+    return tax_price
--- tests/test_taxes.py
+++ tests/test_taxes.py
@@ -3,20 +3,29 @@ import pytest
 from src.taxes import calculate_tax
 
 
-@pytest.mark.parametrize('price, tax_rate, expected', [
-    (100, 10, 110),
-    (200, 20, 240),
-    (300, 30, 390)
+@pytest.mark.parametrize('price, tax_rate, discount, expected', [
+    (100, 10, 20, 88),
+    (200, 20, 30, 168),
+    (300, 30, 40, 234)
 ])
-def test_calculate_tax(price, tax_rate, expected):
-    assert calculate_tax(price, tax_rate) == expected
+def test_calculate_tax_with_discount(price, tax_rate, discount, expected):
+    assert calculate_tax(price, tax_rate, discount) == expected
+
+
+def test_calculate_tax_without_discount():
+    assert calculate_tax(price=100, tax_rate=10) == 110
 
 
 def test_calculate_tax_invalid_prices():
     with pytest.raises(ValueError):
-        calculate_tax(-20, 10)
+        calculate_tax(price=-20, tax_rate=10)
+
+
+def test_calculate_tax_invalid_discount():
+    with pytest.raises(ValueError):
+        calculate_tax(price=-20, tax_rate=10, discount=-10)
 
 
 def test_calculate_tax_valid_tax_rate():
     with pytest.raises(ValueError):
-        calculate_tax(10, -10)
+        calculate_tax(price=10, tax_rate=-10)
